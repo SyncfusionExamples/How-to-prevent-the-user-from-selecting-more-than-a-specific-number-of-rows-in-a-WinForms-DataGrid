bf2218d6eb707b0317bf4731e68089f4ab797adf
SweathaBharathi <sweatha.bharathi@syncfusion.com>
2025-09-04 13:10:05 +0530

    To prevent the user from selecting more than a specific number of rows in a WinForms DataGrid.

---
+++ SfDataGridDemo/Model/Model.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGrid_Demo
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        int orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shipCity;
+        public int OrderID
+        {
+            get { return orderID; }
+            set
+            {
+                orderID = value;
+                OnPropertyChanged("OrderID");
+            }
+        }
+        public string CustomerID
+        {
+            get { return customerId; }
+            set
+            {
+                customerId = value;
+                OnPropertyChanged("CustomerID");
+            }
+        }
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                customerName = value;
+                OnPropertyChanged("CustomerName");
+            }
+        }
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                OnPropertyChanged("Country");
+            }
+        }
+        public string ShipCity
+        {
+            get { return shipCity; }
+            set
+            {
+                shipCity = value;
+                OnPropertyChanged("ShipCity");
+            }
+        }
+
+        public OrderInfo(int orderId, string customerName, string country, string customerId, string shipCity)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+        }
+
+        public OrderInfo()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
